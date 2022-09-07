e26c9f454cb7f61abdadfd3822fb8dc8e576e802
stecala <ste.cala123@gmail.com>
2022-09-07 15:44:26 +0200

    feat: :sparkles: end main component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,20 @@
               <div>CURRENT SERIES</div>
             </div>
             <div class="row g-3">
+                {{-- qui --}}
+                @forelse ($comics as $comic)
+                    <div class="col-2 card">
+                        <div class="img-container">
+                            <img src="{{$comic->image}}" alt="{{$comic->title}}">
+                        </div>
+                        <div class="text-container py-2">
+                            {{$comic->title}}
+                        </div>
+                    </div>
+                    
+                @empty
+                    Non sono presenti fumetti
+                @endforelse
               <div class="btn-cont d-flex justify-content-center">
                 <a class="text-white">LOAD MORE</a>
               </div>
