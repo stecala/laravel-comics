5afd7b16c216c66d1475466e3dc2ae6d8413cb8d
stecala <ste.cala123@gmail.com>
2022-09-07 15:20:48 +0200

    feat: :sparkles: Creation of Header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,48 @@
 <header>
-   <h1>
-       testa
-    </h1> 
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-2 d-flex align-items-center">
+                <img src="/assets/images/dc-logo.png" alt="Main logo DC">
+            </div>
+            <nav class="col-8 d-flex align-items-center ">
+                <ul class="d-flex ">
+                    <li>
+                        characters
+                    </li>
+                    <li>
+                        comics
+                    </li>
+                    <li>
+                        movies
+                    </li>
+                    <li>
+                        tv
+                    </li>
+                    <li>
+                        games
+                    </li>
+                    <li>
+                        collectibles
+                    </li>
+                    <li>
+                        videos
+                    </li>
+                    <li>
+                        fans
+                    </li>
+                    <li>
+                        news
+                    </li>
+                    <li>
+                        shops
+                    </li>
+                </ul>
+            </nav> 
+            <div class="col-2 d-flex align-items-center ">
+                <input type="text" placeholder="Search" >
+                <button class="btn " type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+    </div>
 
 </header>
