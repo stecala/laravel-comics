670c3cd11beca27a9191cc86ae268cdd60bbaf9a
stecala <ste.cala123@gmail.com>
2022-09-07 15:30:21 +0200

    feat: :sparkles: created banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.main')
 @section('home')
-    <main>
-        <h2>main</h2>
+    <main  class="my-bg">
+        <div class="banner-main">
+            <img src="/assets/images/jumbotron.jpg" >
+        </div>
+        <div class="container-lg py-5 position-relative">
+            <div class="section-title position-absolute">
+              <div>CURRENT SERIES</div>
+            </div>
+            <div class="row g-3">
+              <div class="btn-cont d-flex justify-content-center">
+                <a class="text-white">LOAD MORE</a>
+              </div>
+            </div>
+          </div>
     </main>
 @endsection
