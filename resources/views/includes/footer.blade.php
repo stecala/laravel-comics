329991b018e07b3d303fa90d535179dde3f79c73
stecala <ste.cala123@gmail.com>
2022-09-07 16:23:19 +0200

    feat: :sparkles: end footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -54,4 +54,39 @@
             </div>
         </div>
     </div>
+    <div class="bg-foot position-relative"> 
+        <div class="container-lg ">
+            <div class="row align-items-center ">
+                <div class="col-7">
+                    <a class="mybtn">SING-UP NOW</a>
+                </div>
+                <div class="col-5 d-flex align-items-center justify-content-end">
+                    <h3>FOLLOW US</h3>
+                    <ul class="d-flex align-items-center mt-3">
+                        <li><a href='#'>
+                            <img src="/assets/images/footer-facebook.png" alt="fb">
+                            </a>
+                        </li>
+                        <li><a href='#'>
+                            <img src="/assets/images/footer-twitter.png" alt="twitter">
+                            </a>
+                        </li>
+                        <li><a href='#'>
+                            <img src="/assets/images/footer-youtube.png" alt="yt">
+                            </a>
+                        </li>
+                        <li><a href='#'>
+                            <img src="/assets/images/footer-pinterest.png" alt="pinterest">
+                            </a>
+                        </li>
+                        <li><a href='#'>
+                            <img src="/assets/images/footer-periscope.png" alt="periscope">
+                            </a>
+                        </li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
